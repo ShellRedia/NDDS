5c988e4bb29cd628b48e8327823641c7badf4f3e
ShellRedia <1063634953@qq.com>
2022-12-12 18:38:43 +0800

    1

---
+++ Plugins/NVSceneCapturer/Source/NVSceneCapturer/NVSceneCapturer.Build.cs
@@ -0,0 +1,30 @@
+/*
+* Copyright (c) 2018 NVIDIA Corporation. All rights reserved.
+* This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+* International License.  (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
+*/
+
+using UnrealBuildTool;
+using System.IO;
+ 
+public class NVSceneCapturer : ModuleRules
+{
+	public NVSceneCapturer(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateIncludePaths.AddRange(new string[] { "NVSceneCapturer/Private" });
+ 
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "Json", "JsonUtilities", "InputCore", "RHI", "RenderCore"});
+        PublicDependencyModuleNames.AddRange(new string[] { "MovieSceneCapture", "ImageWrapper" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { "zlib", "UElibPNG" } );
+
+        if (Target.Type == TargetRules.TargetType.Editor)
+        {
+            PublicDependencyModuleNames.Add("UnrealEd");
+        }
+
+        PrivatePCHHeaderFile = "Public/NVSceneCapturerModule.h";
+
+        bFasterWithoutUnity = true;
+    }
+}
---
+++ Plugins/NVUtilities/Source/NVUtilities/NVUtilities.Build.cs
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) 2018 NVIDIA Corporation. All rights reserved.
+* This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+* International License.  (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
+*/
+
+using UnrealBuildTool;
+using System.IO;
+ 
+public class NVUtilities : ModuleRules
+{
+	public NVUtilities(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateIncludePaths.AddRange(new string[] { "NVUtilities/Private" });
+ 
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "Json", "JsonUtilities", "InputCore"});
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        PrivatePCHHeaderFile = "Public/NVUtilitiesModule.h";
+
+        bFasterWithoutUnity = true;
+    }
+}
